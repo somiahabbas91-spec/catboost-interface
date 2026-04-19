c26c0f90b286eec578b4ef70ea30d4dcc02db216
somiahabbas91-spec <somiahabbas91@gmail.com>
2026-04-19 01:13:03 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from unified_interface import CatBoostUnifiedInterface
+# =========================
+# LOAD INTERFACE
+# =========================
+interface = CatBoostUnifiedInterface(
+    clf_model_path=r"D:\data AI\SAMSOUMA\best_catboost_classification.pkl",
+    reg_model_path=r"D:\data AI\SAMSOUMA\best_catboost_regression.pkl",
+    feature_names=[
+        "L(mm)","H1(mm) ","Bf1(mm)",
+        "Bl1(mm)","t1(mm)","R1   ",
+        "e(mm)","a(mm)","k",
+        "p(mm)","d(mm)","LC","Fy(N/mm²)"
+    ],
+    label_names=['L', 'D', 'G', 'L+D', 'L+G', 'FT', 'L+FT']
+)
+
+# =========================
+# UI
+# =========================
+st.title("Structural Failure Prediction System")
+
+st.header("Input Parameters")
+
+L = st.number_input("L (mm)", value=400.0)
+H1 = st.number_input("H1 (mm)", value=50.0)
+Bf1 = st.number_input("Bf1 (mm)", value=20.0)
+Bl1 = st.number_input("Bl1 (mm)", value=10.0)
+t1 = st.number_input("t1 (mm)", value=1.2)
+R1 = st.number_input("R1", value=2.3)
+e = st.number_input("e (mm)", value=25.0)
+a = st.number_input("a (mm)", value=13.0)
+k = st.number_input("k", value=21.0)
+p = st.number_input("p (mm)", value=12.0)
+d = st.number_input("d (mm)", value=11.0)
+LC = st.number_input("LC", value=1.0)
+Fy = st.number_input("Fy (N/mm²)", value=350.0)
+
+# =========================
+# PREDICT BUTTON
+# =========================
+if st.button("Predict"):
+
+    input_data = {
+        "geometry": {
+            "L(mm)": L,
+            "H1(mm) ": H1,
+            "Bf1(mm)": Bf1,
+            "Bl1(mm)": Bl1,
+            "t1(mm)": t1,
+            "R1   ": R1,
+            "e(mm)": e,
+            "a(mm)": a,
+            "k": k,
+            "p(mm)": p,
+            "d(mm)": d,
+        },
+        "limite condition": {
+            "LC": LC,
+        },
+        "material": {
+            "Fy(N/mm²)": Fy,
+        }
+    }
+
+    result = interface.predict_with_confidence(input_data)
+
+    st.success("Prediction completed")
+
+    st.write("Failure mode:", result["failure_mode"])
+    st.write("Confidence:", result["confidence"])
+    st.write("Ultimate load:", result["ultimate_load"])
