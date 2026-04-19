724e7c95ccdf647984ae2a20d004f4098652292c
somiahabbas91-spec <somiahabbas91@gmail.com>
2026-04-19 03:36:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,13 @@ class CatBoostUnifiedInterface:
 # =========================
 # FEATURE LIST (CLEANED)
 # =========================
-feature_names = [
-    "L(mm)","H1(mm)","Bf1(mm)",
-    "Bl1(mm)","t1(mm)","R1",
-    "e(mm)","a(mm)","k",
-    "p(mm)","d(mm)","LC","Fy(N/mm²)"
-]
+feature_names=[
+        "L","H1","Bf1",
+        "Bl1","t1","R1",
+        "e","a","k",
+        "p","d","LC","Fy"
+    ]
+
 
 label_names = ['L', 'D', 'G', 'L+D', 'L+G', 'FT', 'L+FT']
 
@@ -76,45 +77,44 @@ st.title("Structural Failure Prediction System")
 
 st.header("Input Parameters")
 
-L(mm)= st.number_input("L (mm)", value=400.0)
-H1(mm) = st.number_input("H1(mm) ", value=50.0)
-Bf1 (mm)= st.number_input("Bf1 (mm)", value=20.0)
-Bl1 (mm)= st.number_input("Bl1 (mm)", value=10.0)
-t1 (mm)= st.number_input("t1 (mm)", value=1.2)
-R1   = st.number_input("R1   ", value=2.3)
-e (mm)= st.number_input("e (mm)", value=25.0)
-a (mm)= st.number_input("a (mm)", value=13.0)
-k= st.number_input("k", value=21.0)
-p (mm)= st.number_input("p (mm)", value=12.0)
-d (mm)= st.number_input("d (mm)", value=11.0)
-LC= st.number_input("LC", value=1.0)
-Fy(N/mm²)= st.number_input("Fy(N/mm²)", value=350.0)
+L = st.number_input("L", value=400.0)
+H1 = st.number_input("H1", value=50.0)
+Bf1 = st.number_input("Bf1", value=20.0)
+Bl1 = st.number_input("Bl1", value=10.0)
+t1 = st.number_input("t1", value=1.2)
+R1 = st.number_input("R1", value=2.3)
+e = st.number_input("e", value=25.0)
+a = st.number_input("a", value=13.0)
+k = st.number_input("k", value=21.0)
+p = st.number_input("p", value=12.0)
+d = st.number_input("d", value=11.0)
+LC = st.number_input("LC", value=1.0)
+Fy = st.number_input("Fy", value=350.0)
 
 # =========================
 # PREDICT BUTTON
 # =========================
 if st.button("Predict"):
 
-    input_data = {
-        "geometry": {
-            "L(mm)": L,
-            "H1(mm)": H1,
-            "Bf1(mm)": Bf1,
-            "Bl1(mm)": Bl1,
-            "t1(mm)": t1,
+   "geometry": {
+            "L": L,
+            "H1": H1,
+            "Bf1": Bf1,
+            "Bl1": Bl1,
+            "t1": t1,
             "R1": R1,
-            "e(mm)": e,
-            "a(mm)": a,
+            "e": e,
+            "a": a,
             "k": k,
-            "p(mm)": p,
-            "d(mm)": d,
+            "p": p,
+            "d": d,
         },
         "limite condition": {
             "LC": LC,
         },
         "material": {
-            "Fy(N/mm²)": Fy,
-        }
+            "Fy": Fy,
+        },
     }
 
     result = interface.predict_with_confidence(input_data)
